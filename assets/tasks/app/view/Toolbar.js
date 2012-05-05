169df30ac35da02c8eae0f8426601465eacb8511
K4T <hesperis2@gmail.com>
2012-05-05 15:06:38 +0200

    Added Create, Edit, Delete button to toolbar

--- assets/tasks/app/view/Toolbar.js
+++ assets/tasks/app/view/Toolbar.js
@@ -4,6 +4,26 @@ Ext.define('Tasks.view.Toolbar', {
     xtype: 'tasksToolbar',
 
     initComponent: function() {
+        this.items = [
+            {
+                text: 'Create',
+                iconCls: 'create-icon',
+                id: 'create-task-btn'
+            },
+            {
+                text: 'Edit',
+                iconCls: 'edit-tb-icon',
+                id: 'edit-task-btn',
+                disabled: true
+            },
+            {
+                text: 'Delete',
+                iconCls: 'delete-tb-icon',
+                id: 'delete-task-btn',
+                disabled: true
+            }
+        ]
+
         this.callParent();
     }
 
