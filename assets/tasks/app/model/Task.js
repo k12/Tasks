6cc7d69588e9376c6290e879a6ea26cba63fa278
KAT <hesperis2@gmail.com>
2012-05-12 13:30:53 +0200

    Task model and dummy tasks data update

--- assets/tasks/app/model/Task.js
+++ assets/tasks/app/model/Task.js
@@ -2,12 +2,15 @@ Ext.define('Tasks.model.Task', {
     extend: 'Ext.data.Model',
 
     fields: [
-        {name: 'id',        type: 'int'},
-        {name: 'title',     type: 'string'},
-        {name: 'dueDate',   type: 'date',       dateFormat: 'Y-m-d'},
-        {name: 'priority',  type: 'string',     defaultValue: 'None'},
-        {name: 'note',      type: 'string'},
-        {name: 'done',      type: 'boolean',    defaultValue: false}
+        {name: 'id',            type: 'int'},
+        {name: 'title',         type: 'string'},
+        {name: 'dueDate',       type: 'date',       dateFormat: 'Y-m-d'},
+        {name: 'priority',      type: 'string',     defaultValue: 'None'},
+        {name: 'note',          type: 'string'},
+        {name: 'done',          type: 'boolean',    defaultValue: false},
+        {name: 'categoryId',    type: 'int'},
+        {name: 'assignedToId',  type: 'int'},
+        {name: 'assignedById',  type: 'int'}
     ],
 
     proxy: {
