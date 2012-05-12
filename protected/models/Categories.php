bb25adb8c6adaf0be1036057b214bf6770e5368a
KAT <hesperis2@gmail.com>
2012-05-12 18:19:16 +0200

    Reformated code for Yii models

--- protected/models/Categories.php
+++ protected/models/Categories.php
@@ -12,79 +12,58 @@
  */
 class Categories extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Categories the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'categories';
-	}
+    public function tableName()
+    {
+        return 'categories';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('category', 'required'),
-			array('category', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, category', 'safe', 'on'=>'search'),
-		);
-	}
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('category', 'required'),
+            array('category', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, category', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'tasks' => array(self::HAS_MANY, 'Tasks', 'categoryId'),
-		);
-	}
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'tasks' => array(self::HAS_MANY, 'Tasks', 'categoryId'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'category' => 'Category',
-		);
-	}
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'category' => 'Category',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('category',$this->category,true);
+        $criteria->compare('id', $this->id);
+        $criteria->compare('category', $this->category, true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 }
--- protected/models/Tasks.php
+++ protected/models/Tasks.php
@@ -21,98 +21,77 @@
  */
 class Tasks extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Tasks the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'tasks';
-	}
+    public function tableName()
+    {
+        return 'tasks';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('title', 'required'),
-			array('done, assignedToId, assignedById, categoryId', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>255),
-			array('priority', 'length', 'max'=>64),
-			array('dueDate, note', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, title, dueDate, priority, note, done, assignedToId, assignedById, categoryId', 'safe', 'on'=>'search'),
-		);
-	}
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('title', 'required'),
+            array('done, assignedToId, assignedById, categoryId', 'numerical', 'integerOnly' => true),
+            array('title', 'length', 'max' => 255),
+            array('priority', 'length', 'max' => 64),
+            array('dueDate, note', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, title, dueDate, priority, note, done, assignedToId, assignedById, categoryId', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'category' => array(self::BELONGS_TO, 'Categories', 'categoryId'),
-			'assignedTo' => array(self::BELONGS_TO, 'Users', 'assignedToId'),
-			'assignedBy' => array(self::BELONGS_TO, 'Users', 'assignedById'),
-		);
-	}
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'category' => array(self::BELONGS_TO, 'Categories', 'categoryId'),
+            'assignedTo' => array(self::BELONGS_TO, 'Users', 'assignedToId'),
+            'assignedBy' => array(self::BELONGS_TO, 'Users', 'assignedById'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'title' => 'Title',
-			'dueDate' => 'Due Date',
-			'priority' => 'Priority',
-			'note' => 'Note',
-			'done' => 'Done',
-			'assignedToId' => 'Assigned To',
-			'assignedById' => 'Assigned By',
-			'categoryId' => 'Category',
-		);
-	}
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'title' => 'Title',
+            'dueDate' => 'Due Date',
+            'priority' => 'Priority',
+            'note' => 'Note',
+            'done' => 'Done',
+            'assignedToId' => 'Assigned To',
+            'assignedById' => 'Assigned By',
+            'categoryId' => 'Category',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('dueDate',$this->dueDate,true);
-		$criteria->compare('priority',$this->priority,true);
-		$criteria->compare('note',$this->note,true);
-		$criteria->compare('done',$this->done);
-		$criteria->compare('assignedToId',$this->assignedToId);
-		$criteria->compare('assignedById',$this->assignedById);
-		$criteria->compare('categoryId',$this->categoryId);
+        $criteria->compare('id', $this->id);
+        $criteria->compare('title', $this->title, true);
+        $criteria->compare('dueDate', $this->dueDate, true);
+        $criteria->compare('priority', $this->priority, true);
+        $criteria->compare('note', $this->note, true);
+        $criteria->compare('done', $this->done);
+        $criteria->compare('assignedToId', $this->assignedToId);
+        $criteria->compare('assignedById', $this->assignedById);
+        $criteria->compare('categoryId', $this->categoryId);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 }
