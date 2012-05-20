9288fda306a4e34cc156d54073d22a25f6f48468
KAT <hesperis2@gmail.com>
2012-05-20 19:54:31 +0200

    schema.sql update

--- protected/data/schema.sql
+++ protected/data/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 12 May 2012, 17:53
+-- Czas wygenerowania: 20 May 2012, 19:53
 -- Wersja serwera: 5.5.16
 -- Wersja PHP: 5.3.8
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(255) NOT NULL,
+  `editable` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
@@ -36,11 +37,11 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Zrzut danych tabeli `categories`
 --
 
-INSERT INTO `categories` (`id`, `category`) VALUES
-(1, 'Private'),
-(2, 'Company'),
-(3, 'Assigned To Me'),
-(4, 'Assigned To Others');
+INSERT INTO `categories` (`id`, `category`, `editable`) VALUES
+(1, 'Private', 0),
+(2, 'Company', 0),
+(3, 'Assigned To Others', 0),
+(4, 'Assigned To Me', 0);
 
 -- --------------------------------------------------------
 
@@ -52,9 +53,10 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `dueDate` date DEFAULT NULL,
-  `priority` varchar(64) NOT NULL DEFAULT 'None',
+  `priority` enum('none','low','normal','high') NOT NULL DEFAULT 'none',
   `note` text,
-  `done` tinyint(1) NOT NULL DEFAULT '0',
+  `state` enum('assigned','in progress','completed') NOT NULL DEFAULT 'assigned',
+  `completedAt` date DEFAULT NULL,
   `assignedToId` int(11) DEFAULT NULL,
   `assignedById` int(11) DEFAULT NULL,
   `categoryId` int(11) DEFAULT NULL,
@@ -74,7 +76,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `name`) VALUES
+(1, 'Jan Kowalski'),
+(2, 'Marta Nowak');
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -84,9 +94,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Ograniczenia dla tabeli `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`assignedToId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`assignedById`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`assignedById`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
