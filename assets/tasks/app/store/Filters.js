194e66a3b07fd35f3962eb7b472914b6efb27097
KAT <hesperis2@gmail.com>
2012-05-30 16:33:58 +0200

    Rearranged Due Date filters

--- assets/tasks/app/store/Filters.js
+++ assets/tasks/app/store/Filters.js
@@ -7,15 +7,15 @@ Ext.define('Tasks.store.Filters', {
     autoLoad: true,
 
     data : [
-        {id: 0, filter: 'All',            type: 'Others'},
-        {id: 1, filter: 'Not started',    type: 'State'},
-        {id: 2, filter: 'In progress',    type: 'State'},
-        {id: 3, filter: 'Completed',      type: 'State'},
-        {id: 4, filter: 'Today',          type: 'Due Date'},
-        {id: 5, filter: 'Tomorrow',       type: 'Due Date'},
-        {id: 6, filter: 'This week',      type: 'Due Date'},
-        {id: 7, filter: 'Future',         type: 'Due Date'},
-        {id: 8, filter: 'Overdue',        type: 'Due Date'},
-        {id: 9, filter: 'Without',        type: 'Due Date'}
+        {filter: 'All',            type: 'Others'},
+        {filter: 'Without',        type: 'Due Date'},
+        {filter: 'Overdue',        type: 'Due Date'},
+        {filter: 'Today',          type: 'Due Date'},
+        {filter: 'Tomorrow',       type: 'Due Date'},
+        {filter: 'This week',      type: 'Due Date'},
+        {filter: 'Future',         type: 'Due Date'},
+        {filter: 'Not started',    type: 'State'},
+        {filter: 'In progress',    type: 'State'},
+        {filter: 'Completed',      type: 'State'}
     ]
 });
