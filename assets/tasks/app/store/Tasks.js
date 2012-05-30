4e439883fe6dfe6aca60a18161e111e556a1df09
KAT <hesperis2@gmail.com>
2012-05-30 16:42:08 +0200

    Removed unnecessary filters from tasks store

--- assets/tasks/app/store/Tasks.js
+++ assets/tasks/app/store/Tasks.js
@@ -3,11 +3,5 @@ Ext.define('Tasks.store.Tasks', {
 
     model: 'Tasks.model.Task',
 
-    filters: [{
-        property: 'state',
-        value: 'in progress'
-    }],
-
     autoLoad: true
-
 });
